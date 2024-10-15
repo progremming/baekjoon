03cdc55f8f8e378f9656b790f3af23e51d87fbd4
progremming <165005066+progremming@users.noreply.github.com>
2024-10-15 11:15:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 84.88 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+
+def solution(s):
+    stack = []  
+
+    for char in s:
+
+        if stack and stack[-1] == char:
+
+            stack.pop() 
+        else:
+            stack.append(char)  
+
+
+    return 1 if not stack else 0
